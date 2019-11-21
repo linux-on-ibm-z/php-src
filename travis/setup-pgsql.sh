fdd4c0d93bba8332dcce23ccba2a92eb78a6b27b
srajmane <31947381+srajmane@users.noreply.github.com>
2019-11-21 18:18:47 +0530

    Update setup-pgsql.sh

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -3,5 +3,6 @@ echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
 if [ -z "$ARM64" ]; then
   psql --version
+  sudo systemctl status postgresql@9.5-main
   psql -c 'create database test;' -U postgres
 fi
