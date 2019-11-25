fdc5478b9e311c803f787836d1ce9791566c3756
Prasanna Kelkar <Prasanna.Kelkar@ibm.com>
2019-11-25 15:45:56 +0530

    debug postgres

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -3,6 +3,11 @@ echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
 if [ -z "$ARM64" ]; then
   echo "inside pgsql if condition"
-  echo $ARM64
+  echo $ARM64 
+  echo $USER
+  psql --version
+  sudo service postgresql start
+  pg_lsclusters
+
   psql -c 'create database test;' -U postgres
 fi
