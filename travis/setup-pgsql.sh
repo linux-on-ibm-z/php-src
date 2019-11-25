d19c2accf40264ef46ce074a80adeabcac0775a8
Prasanna Kelkar <Prasanna.Kelkar@ibm.com>
2019-11-25 16:23:46 +0530

    pgsql 9.5

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -5,9 +5,11 @@ if [ -z "$ARM64" ]; then
   echo "inside pgsql if condition"
   echo $ARM64 
   echo $USER
+  echo $s390x
   psql --version
   sudo systemctl start postgresql@9.5-main
-  sudo service postgresql start
+  sudo systemctl status postgresql@9.5-main
+  
   pg_lsclusters
 
   psql -c 'create database test;' -U postgres
