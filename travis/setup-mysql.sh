4430c1b7b572399e388af426f3e18e997ed3a1b2
Prasanna Kelkar <Prasanna.Kelkar@ibm.com>
2019-11-25 15:34:04 +0530

    debug arm64 variable

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [ -n "$ARM64" ]; then
+  echo "inside mysql if condition"
+  echo $ARM64
   sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
   sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
 fi
--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -2,6 +2,7 @@
 echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
 if [ -z "$ARM64" ]; then
+  echo "inside pgsql if condition"
+  echo $ARM64
   psql -c 'create database test;' -U postgres
 fi
-psql -c 'create database test;' -U postgres
