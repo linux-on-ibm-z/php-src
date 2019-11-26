e032bedcd07ac6e3c9e19d6a7d802ab6214958cc
srajmane <31947381+srajmane@users.noreply.github.com>
2019-11-26 11:18:08 +0530

    Update setup-pgsql.sh

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -3,7 +3,6 @@ echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
 if [ -z "$ARM64" ]; then
   psql --version
-  sudo /etc/init.d/postgresql start
   sudo which pg_lsclusters
   sudo ls -la /var/log/postgres/*
   sudo cat /var/log/postgresql-9.5-main.log
