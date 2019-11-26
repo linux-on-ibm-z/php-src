e09ade9150c4e0a06df4b58aee6af5330fdff271
srajmane <31947381+srajmane@users.noreply.github.com>
2019-11-26 10:37:16 +0530

    Update setup-pgsql.sh

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -3,9 +3,9 @@ echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
 if [ -z "$ARM64" ]; then
   psql --version
-  sudo service postgresql start
-  pg_lsclusters
-  sudo ls -la /var/log/postgres
+  sudo /etc/init.d/postgresql start
+  sudo which pg_lsclusters
+  sudo ls -la /var/log/postgres*
   sudo cat /var/log/postgresql/postgresql-9.5-main.log
   psql -c 'create database test;' -U postgres
 fi
